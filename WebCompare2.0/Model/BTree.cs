64584340ea35e32798c9baf049751e121595aaeb
Rick Wallace <rick.wallace@vareximaging.com>
2017-10-25 19:07:18 -0400

    Updated BTree Read and Write methods

--- WebCompare2.0/Model/BTree.cs
+++ WebCompare2.0/Model/BTree.cs
@@ -11,9 +11,11 @@ namespace WebCompare2._0.Model
 {
     public class BTree
     {
-        const int k = 100;
+        const int ByteLength = 100;
+        const int k = 16;
         static int numOfNodes = 0;
         static Node root;
+        long id;
 
         /// <summary>
         /// Constructor.
@@ -21,8 +23,9 @@ namespace WebCompare2._0.Model
         /// <param name="id">Root </param>
         public BTree()
         {
+            id = 0;
             root = new Node(null);
-            WriteNode(root);
+            WriteTree(root);
         }
 
         /// <summary>
@@ -32,50 +35,117 @@ namespace WebCompare2._0.Model
         public class Node
         {
             long id;
-            KeyValuePair<long, string>[] kvPairs;
-            Node[] children;
-            Node parent;
+            public KeyValuePair<float, string>[] kvPairs;
+            long[] children;
+            long parent;
             bool leaf;
 
             public Node(Node p)
             {
-                id = ++numOfNodes;
-                kvPairs = new KeyValuePair<long, string>[k - 1];
-                children = new Node[k];
-                parent = p;
+                id = 0;
+                kvPairs = new KeyValuePair<float, string>[k];
+                children = new long[k + 1];
                 leaf = true;
+                ++numOfNodes;
             }
-
-            // Name property
-            public long ID
+         #region Node Properties 
+         // ID property
+         public long ID
             {
                 get
                 {
                     return id;
                 }
+                set
+                {
+                    id = value;
+                }
             }
-        }
+           // Name property
+            public KeyValuePair<float, string>[] KVPairs
+            {
+                get
+                {
+                    return kvPairs;
+                }
+                set
+                {
+                    kvPairs = value;
+                }
+            }
+            // Name property
+            public long[] Children
+            {
+                get
+                {
+                    return children;
+                }
+                set
+                {
+                    children = value;
+                }
+            }
+            // Name property
+            public long Parent
+            {
+                get
+                {
+                    return parent;
+                }
+                set
+                {
+                    parent = value;
+                }
+            }
+            // Name property
+            public bool Leaf
+            {
+                get
+                {
+                    return leaf;
+                }
+                set
+                {
+                    leaf = value;
+                }
+            }
+         #endregion
+      }
 
-        /// <summary>
-        /// Read a Node from disk
-        /// </summary>
-        /// <param name="id">Node identification number.</param>
-        public Node ReadNode(long id)
+      /// <summary>
+      /// Read a Node from disk
+      /// </summary>
+      /// <param name="id">Node identification number.</param>
+      public static Node ReadTree(long id)
         {
-            string FileName = $"treebin\\node{id}.bin";
+            string FileName = $"treebin\\tree.bin";
+            FileStream fs = null;
+            Node readNode = null;
             try
             {
                 if (File.Exists(FileName))
                 {
-                    Stream filestream = File.OpenRead(FileName);
-                    BinaryFormatter deserializer = new BinaryFormatter();
-                    Node node = (Node) deserializer.Deserialize(filestream);
-                    filestream.Close();
-                    return node;
-                }
+                    readNode.KVPairs = new KeyValuePair<float, string>[k];
+                    using (BinaryReader reader = new BinaryReader(File.Open(FileName, FileMode.Open)))
+                    {
+                       readNode.Leaf = reader.ReadBoolean();
+                       readNode.Parent = reader.ReadInt64();
+                       for (int kvi = 0; kvi <= k; ++kvi)
+                       {
+                            float key = reader.ReadSingle();
+                            string val = reader.ReadString();
+                            readNode.KVPairs[kvi] = new KeyValuePair<float, string>(key, val);
+                       }
+                       for (int ci = 0; ci <= (k+1); ++ci)
+                       {
+                           readNode.Children[ci] = reader.ReadInt64();
+                       }
+                    } // end using
+                } // end if
             }
             catch (Exception e) { Console.WriteLine("Error in ReadNode: " + e); }
-            return null;
+            finally { fs.Close(); }
+            return readNode;
         }
 
 
@@ -84,17 +154,34 @@ namespace WebCompare2._0.Model
         /// </summary>
         /// <param name="node">Node to be written.</param>
         /// <param name="id">Node identification number.</param>
-        public void WriteTree()
+        public void WriteTree(Node node)
         {
-            try
+         string FileName = $"treebin\\tree.bin";
+         FileStream fs = new FileStream(FileName, FileMode.Append, FileAccess.Write);
+         try
             {
-                string FileName = $"treebin\\tree.bin";
-                Stream TestFileStream = File.Create(FileName);
-                BinaryFormatter serializer = new BinaryFormatter();
-                serializer.Serialize(TestFileStream, this);
-                TestFileStream.Close();
+                MemoryStream ms = new MemoryStream();
+                using (BinaryWriter writer = new BinaryWriter(ms))
+                {
+                    writer.Write(node.Leaf);
+                    writer.Write(node.Parent);
+                    for (int kvi = 0; kvi <= k; ++kvi)
+                    {
+                         writer.Write(node.KVPairs[kvi].Key);
+                         writer.Write(node.KVPairs[kvi].Value);
+                    }
+                    for (int ci = 0; ci <= (k+1); ++ci)
+                    {
+                        writer.Write(node.Children[ci]);
+                    }
+                    
+                }
+                byte[] bytes = ms.ToArray();
+                fs.Write(bytes, (int)fs.Position, ByteLength);  // write the stream to file, already should be at the end
+                fs.Close();
             }
             catch (Exception e) { Console.WriteLine("Error in WriteNode: " + e); }
+            finally { fs.Close(); }
         }
 
 
